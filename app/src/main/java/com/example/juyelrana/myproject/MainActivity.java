03229773ffb35ba870491832a990e47728bc69a8
DreamFactory1312 <dreamfactory1312@gmail.com>
2019-01-15 11:33:28 +0600

    second commit

--- app/src/main/java/com/example/juyelrana/myproject/MainActivity.java
+++ app/src/main/java/com/example/juyelrana/myproject/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Juyel Rana", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Juyel Rana Toast", Toast.LENGTH_SHORT).show();
 
     }
 }
